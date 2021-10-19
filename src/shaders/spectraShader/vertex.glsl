96511c1b18449aad39691642cc73796f40e038bd
Ada Toydemir <Ada Toydemir>
2021-10-18 20:35:33 -0700

    getting there

--- src/shaders/spectraShader/vertex.glsl
+++ src/shaders/spectraShader/vertex.glsl
@@ -1,9 +1,10 @@
 attribute float displacement;
-uniform vec3 vLut[1];
+uniform vec3 vLut[256];
 varying vec3 vColor;
 
 void main(){
-    vColor = vLut[0];
+    int index = int(displacement);
+    vColor = vLut[index];
     vec3 newPosition = position + normal*displacement/25.5;
     gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition,1.0);
 }
